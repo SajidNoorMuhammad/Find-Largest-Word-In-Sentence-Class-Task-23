b8047208d6eecb9883b494246fd31a47d59a2724
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-07-10 13:21:28 +0500

    add prompt

---
+++ app.js
@@ -0,0 +1,17 @@
+let sentence = prompt("Enter Text Here");
+
+// function findLargestWord(sentence) {
+//     let words = sentence.split(' ');
+//     let largestWord = '';
+
+//     for (let i = 0; i < words.length; i++) {
+//         if (words[i].length > largestWord.length) {
+//             largestWord = words[i];
+//         }
+//     }
+
+//     return largestWord;
+// }
+
+// let largestWord = findLargestWord(sentence);
+// console.log("The largest word is:", largestWord);
